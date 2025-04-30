6b963356d44a8eca12fa78b05a16f90b47634ae1
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2025-04-30 15:36:53 +0700

    feat: adding env config

--- internal/bootstrap/env.go
+++ internal/bootstrap/env.go
@@ -1,20 +1,14 @@
 package bootstrap
 
 import (
-	"log"
-
 	"github.com/spf13/viper"
+	"github.com/TranThang-2804/infrastructure-engine/internal/shared/log"
 )
 
 type Env struct {
 	AppEnv                 string `mapstructure:"APP_ENV"`
 	ServerAddress          string `mapstructure:"SERVER_ADDRESS"`
 	ContextTimeout         int    `mapstructure:"CONTEXT_TIMEOUT"`
-	DBHost                 string `mapstructure:"DB_HOST"`
-	DBPort                 string `mapstructure:"DB_PORT"`
-	DBUser                 string `mapstructure:"DB_USER"`
-	DBPass                 string `mapstructure:"DB_PASS"`
-	DBName                 string `mapstructure:"DB_NAME"`
 	AccessTokenExpiryHour  int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
 	RefreshTokenExpiryHour int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
 	AccessTokenSecret      string `mapstructure:"ACCESS_TOKEN_SECRET"`
@@ -27,16 +21,16 @@ func NewEnv() *Env {
 
 	err := viper.ReadInConfig()
 	if err != nil {
-		log.Fatal("Can't find the file .env : ", err)
+		log.Logger.Fatal("Can't find the file .env : ", "err", err)
 	}
 
 	err = viper.Unmarshal(&env)
 	if err != nil {
-		log.Fatal("Environment can't be loaded: ", err)
+		log.Logger.Fatal("Environment can't be loaded: ", "err", err)
 	}
 
 	if env.AppEnv == "development" {
-		log.Println("The App is running in development env")
+	  log.Logger.Info("The App is running in development env")
 	}
 
 	return &env
