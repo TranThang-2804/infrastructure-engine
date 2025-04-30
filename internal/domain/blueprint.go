f26ff8764ef825fae01ffff71e46178ea8845e8f
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2025-04-30 21:29:51 +0700

    feat(blueprint): modifying domain

--- internal/domain/blueprint.go
+++ internal/domain/blueprint.go
@@ -7,25 +7,25 @@ import (
 )
 
 type BluePrint struct {
-	Name            string            `json:"name" yaml:"name"`
-	Id              string            `json:"id" yaml:"id"`
-	Description     string            `json:"description" yaml:"description"`
-	Provider        constant.Provider `json:"provider" yaml:"provider"`
-	IconImageUrl    string            `json:"iconImageUrl" yaml:"iconImageUrl"`
-	Versions        []Version         `json:"versions" yaml:"versions"`
-	Valid           bool              `json:"valid,omitempty" yaml:"valid,omitempty"`
-	ValidationError string            `json:"validationError,omitempty" yaml:"validationError,omitempty"`
+	Name            string             `json:"name" yaml:"name"`
+	Id              string             `json:"id" yaml:"id"`
+	Description     string             `json:"description" yaml:"description"`
+	Provider        constant.Provider  `json:"provider" yaml:"provider"`
+	IconImageUrl    string             `json:"iconImageUrl" yaml:"iconImageUrl"`
+	Versions        []BluePrintVersion `json:"versions" yaml:"versions"`
+	Valid           bool               `json:"valid,omitempty" yaml:"valid,omitempty"`
+	ValidationError string             `json:"validationError,omitempty" yaml:"validationError,omitempty"`
 }
 
-type Version struct {
-	Name                 string              `json:"name" yaml:"name"`
-	JsonSchema           string              `json:"jsonSchema" yaml:"jsonSchema"`
-	JsonSchemaForEditing string              `json:"jsonSchemaForEditing" yaml:"jsonSchemaForEditing"`
-	UiSchema             string              `json:"uiSchema,omitempty" yaml:"uiSchema,omitempty"`
-	CompositeTemplate    []CompositeTemplate `json:"compositeTemplate" yaml:"compositeTemplate"`
+type BluePrintVersion struct {
+	Name                 string                       `json:"name" yaml:"name"`
+	JsonSchema           string                       `json:"jsonSchema" yaml:"jsonSchema"`
+	JsonSchemaForEditing string                       `json:"jsonSchemaForEditing" yaml:"jsonSchemaForEditing"`
+	UiSchema             string                       `json:"uiSchema,omitempty" yaml:"uiSchema,omitempty"`
+	CompositeTemplate    []BluePrintCompositeTemplate `json:"compositeTemplate" yaml:"compositeTemplate"`
 }
 
-type CompositeTemplate struct {
+type BluePrintCompositeTemplate struct {
 	TemplateId    string `json:"templateId" yaml:"templateId"`
 	Version       string `json:"version" yaml:"version"`
 	ValueTemplate string `json:"valueTemplate" yaml:"valueTemplate"`
--- internal/domain/iac-template.go
+++ internal/domain/iac-template.go
@@ -3,11 +3,18 @@ package domain
 import "context"
 
 type IacTemplate struct {
-	Name        string `json:"name"`
-	Version     string `json:"version"`
-	Description string `json:"description"`
-	Provider    string `json:"provider"`
-	Url         string `json:"url"`
+	Name        string               `json:"name"`
+  Id          string               `json:"id"`
+	Description string               `json:"description"`
+	Provider    string               `json:"provider"`
+	Versions    []IacTemplateVersion `json:"versions"`
+}
+
+type IacTemplateVersion struct {
+	VersionName string `json:"versionName"`
+	GitUrl      string `json:"gitUrl"`
+	Path        string `json:"path"`
+	Branch      string `json:"branch"`
 }
 
 type IacTemplateRepository interface {
