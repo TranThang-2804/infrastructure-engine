2b9980ababdfaa2529158047b3bb4d900c3ac011
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2025-05-03 18:14:42 +0700

    feat: adding yaml configuration for composite resource type

--- internal/domain/composite-resource.go
+++ internal/domain/composite-resource.go
@@ -7,59 +7,59 @@ import (
 )
 
 type Resource struct {
-	Name               string                  `json:"name" validate:"required"`
-	Id                 string                  `json:"id" validate:"required"`
-	Status             constant.ResourceStatus `json:"status" validate:"required"`
-	Description        string                  `json:"description" validate:"required"`
-	IacTemplateId      string                  `json:"iacTemplateId" validate:"required"`
-	IacTemplateVersion string                  `json:"iacTemplateVersion" validate:"required"`
-	ResourceValue      string                  `json:"spec" validate:"required"`
-	RunId              []string                `json:"runId"`
+	Name               string                  `json:"name" yaml:"name" validate:"required"`
+	Id                 string                  `json:"id" yaml:"id" validate:"required"`
+	Status             constant.ResourceStatus `json:"status" yaml:"status" validate:"required"`
+	Description        string                  `json:"description" yaml:"description" validate:"required"`
+	IacTemplateId      string                  `json:"iacTemplateId" yaml:"iacTemplateId" validate:"required"`
+	IacTemplateVersion string                  `json:"iacTemplateVersion" yaml:"iacTemplateVersion" validate:"required"`
+	ResourceValue      string                  `json:"spec" yaml:"spec" validate:"required"`
+	RunId              []string                `json:"runId" yaml:"runId"`
 }
 
 type CompositeResource struct {
-	Name             string                    `json:"name" validate:"required"`
-	Id               string                    `json:"id" validate:"required"`
-	Description      string                    `json:"description" validate:"required"`
-	BluePrintId      string                    `json:"bluePrintId" validate:"required"`
-	BluePrintVersion string                    `json:"bluePrintVersion" validate:"required"`
-	CreatedAt        string                    `json:"createdAt" validate:"required"`
-	CreatedBy        string                    `json:"createdBy" validate:"required"`
-	LastModifiedAt   string                    `json:"lastModifiedAt" validate:"required"`
-	LastModifiedBy   string                    `json:"lastModifiedBy" validate:"required"`
-	Spec             map[string]interface{}    `json:"spec" validate:"required"`
-	Status           constant.ResourceStatus   `json:"status" validate:"required"`
-	Resources        []Resource                `json:"resources" validate:"required"`
-	Metadata         CompositeResourceMetadata `json:"metadata,omitempty"`
+	Name             string                    `json:"name" yaml:"name" validate:"required"`
+	Id               string                    `json:"id" yaml:"id" validate:"required"`
+	Description      string                    `json:"description" yaml:"description" validate:"required"`
+	BluePrintId      string                    `json:"bluePrintId" yaml:"bluePrintId" validate:"required"`
+	BluePrintVersion string                    `json:"bluePrintVersion" yaml:"bluePrintVersion" validate:"required"`
+	CreatedAt        string                    `json:"createdAt" yaml:"createdAt" validate:"required"`
+	CreatedBy        string                    `json:"createdBy" yaml:"createdBy" validate:"required"`
+	LastModifiedAt   string                    `json:"lastModifiedAt" yaml:"lastModifiedAt" validate:"required"`
+	LastModifiedBy   string                    `json:"lastModifiedBy" yaml:"lastModifiedBy" validate:"required"`
+	Spec             map[string]interface{}    `json:"spec" yaml:"spec" validate:"required"`
+	Status           constant.ResourceStatus   `json:"status" yaml:"status" validate:"required"`
+	Resources        []Resource                `json:"resources" yaml:"resources" validate:"required"`
+	Metadata         CompositeResourceMetadata `json:"metadata,omitempty" yaml:"metadata,omitempty"`
 }
 
 type CompositeResourceMetadata struct {
-	Group   string `json:"group"`
-	Project string `json:"project"`
+	Group   string `json:"group" yaml:"group"`
+	Project string `json:"project" yaml:"project"`
 }
 
 type GetCompositeResourceRequest struct {
-	Name          string `json:"name,omitempty"`
-	BluePrintType string `json:"bluePrintType,omitempty"`
-	Id            string `json:"id,omitempty"`
+  Name          string `json:"name,omitempty" yaml:"name,omitempty" validate:"required"`
+  BluePrintType string `json:"bluePrintType,omitempty" yaml:"bluePrintType,omitempty" validate:"required"`
+  Id            string `json:"id,omitempty" yaml:"bluePrintType,omitempty" validate:"required"`
 }
 
 type GetCompositeResourceResponse struct {
-	CompositeResource []CompositeResource `json:"compositeResources"`
+	CompositeResource []CompositeResource `json:"compositeResources" yaml:"compositeResources"`
 }
 
 type CreateCompositeResourceRequest struct {
-	Name             string                    `json:"name" validate:"required"`
-	Description      string                    `json:"description" validate:"required"`
-	Spec             map[string]interface{}    `json:"spec" validate:"required"`
-	BluePrintId      string                    `json:"bluePrintId" validate:"required"`
-	BluePrintVersion string                    `json:"bluePrintVersion" validate:"required"`
-	MetaData         CompositeResourceMetadata `json:"metadata,omitempty"`
+  Name             string                    `json:"name" yaml:"name" validate:"required"`
+  Description      string                    `json:"description" yaml:"description" validate:"required"`
+  Spec             map[string]interface{}    `json:"spec" yaml:"spec" validate:"required"`
+  BluePrintId      string                    `json:"bluePrintId" yaml:"bluePrintId" validate:"required"`
+  BluePrintVersion string                    `json:"bluePrintVersion" yaml:"bluePrintVersion" validate:"required"`
+  MetaData         CompositeResourceMetadata `json:"metadata,omitempty" yaml:"metadata,omitempty"`
 }
 
 type CreateCompositeResourceResponse struct {
-	CompositeResource CompositeResource `json:"compositeResource"`
-	Status            string            `json:"status"`
+	CompositeResource CompositeResource `json:"compositeResource" yaml:"compositeResource"`
+	Status            string            `json:"status" yaml:"status"`
 }
 
 type CompositeResourceRepository interface {
